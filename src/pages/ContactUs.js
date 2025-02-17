9c38d42366771499abe49a3196b79309b7f4db1a
sumit bhagat <sumitbhagat3228@gmail.com>
2025-02-17 15:10:29 +0530

    made new sections in contact us page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -9,9 +9,15 @@ const ContactUs = () => (
   <main className="contact-page">
     <h1>Contact Us</h1>
     <p>If you have any questions or need assistance, feel free to reach out to our team below.</p>
-    <ContactSection name="Aryan Goyal" email="aryangoyalbusiness@gmail.com" phone="+91 6396667308" imgSrc={man} />
-    <ContactSection name="Tejasvhi Singh" email="Tejayashsingh@gmail.com" phone="+91 6394468891" imgSrc={manager} />
-    <ContactSection name="Prince Pratap" email="princepratapism@gmail.com" phone="+91 7668629313" imgSrc={boy} />
+    <section className="contact-section">
+      <ContactSection name="Aryan Goyal" email="aryangoyalbusiness@gmail.com" phone="+91 6396667308" imgSrc={man} />
+    </section>
+    <section className="contact-section">
+      <ContactSection name="Tejasvhi Singh" email="Tejayashsingh@gmail.com" phone="+91 6394468891" imgSrc={manager} />
+    </section>
+    <section className="contact-section">
+      <ContactSection name="Prince Pratap" email="princepratapism@gmail.com" phone="+91 7668629313" imgSrc={boy} />
+    </section>
   </main>
 );
 
