b431dac43a1a644f913368644e0c8fd0348deb67
sumit bhagat <sumitbhagat3228@gmail.com>
2025-02-18 08:44:14 +0530

    Add image asset inva.png to the project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import './Home.css'; // Make sure to create and import the CSS file
-import introVideo from '../assets/intro.mp4'; // Import the video file
+import './Home.css';
+import introVideo from '../assets/intro.mp4'; 
+import inva from '../assets/inva.png';
 
 const Home = () => (
   <main>
@@ -18,8 +19,8 @@ const Home = () => (
     <section className="trusted-section">
       <h2>Trusted and Mentored By</h2>
       <div className="company">
-        <img src="path/to/company-logo.png" alt="Company Name" />
-        <p>Company Name</p>
+        <img src={inva} alt="Company Name" />
+        <p>Invasystems</p>
       </div>
     </section>
   </main>
