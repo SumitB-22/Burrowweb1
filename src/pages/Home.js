17dafe8a445462310e188060d10fb581d5ca0444
sumit bhagat <sumitbhagat3228@gmail.com>
2025-02-18 09:10:36 +0530

    color changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,6 @@ import inva from '../assets/inva.png';
 const Home = () => (
   <main>
     <Hero />
-    <section className="video-section">
-      <h2>Our Introduction Video</h2>
-      <div className="video-container">
-        <video width="560" height="315" controls>
-          <source src={introVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </section>
     <section className="trusted-section">
       <h2>Trusted and Mentored By</h2>
       <div className="company">
